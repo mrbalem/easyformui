32b75598ada8b3c3aab5d262548b69f8e14902f2
Rony cb <mylovepre16@gmail.com>
2020-06-25 22:45:57 -0500

    adding export inpusts

---
+++ src/components/inputs/components/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * @author Rony cb
+ * @version 1.0.0
+ * export component
+ */
+
+export { default as CheckBox } from "./checkbox";
+export { default as Select } from "./select";
+export { default as SelectMultiple } from "./selectmultiple";
+export { default as TextField } from "./textfield";
+export { default as InputFile } from "./inputfile";
