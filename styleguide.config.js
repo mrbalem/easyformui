fb45324cbf1c4cf2b873ba8741dfa9c374f5cc1d
Rony cb <mylovepre16@gmail.com>
2020-06-25 22:43:36 -0500

    adding parse component -> forms

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,4 +2,5 @@ module.exports = {
   propsParser: require("react-docgen-typescript").withCustomConfig(
     "./tsconfig.json"
   ).parse,
+  components: "src/components/forms/*.{js,jsx,ts,tsx}",
 };
