50b72f4e54aefd418ece0eddac1d59ec33800217
Rony cb <mylovepre16@gmail.com>
2020-06-25 22:45:29 -0500

    adding types for components -> inpust

---
+++ src/components/inputs/type.ts
@@ -0,0 +1,24 @@
+/**
+ * interface forms
+ */
+import { formtype, FormsProps } from "../forms";
+
+/**
+ * omit props OF FormsProps
+ */
+type omitpProps = Omit<FormsProps, "form" | "onSubmit" | "children">;
+
+/**
+ * types props for input Component
+ * @author Rony cb
+ * @version 1.0.0
+ * @param value necesarie for getInput
+ * @param erros error capture for input;
+ * @param touched If touch input defaul checked errors
+ */
+export interface InputsProps extends omitpProps {
+  value: formtype;
+  errors: any;
+  touched: any;
+  valuesInput: any;
+}
